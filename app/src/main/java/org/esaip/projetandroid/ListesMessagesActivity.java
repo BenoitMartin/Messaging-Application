ae3f95e0fe6064c747a3afecfda873e6dfb265ab
BenoitMartin <martbenoit@gmail.com>
2015-01-26 12:42:05 +0100

    Update : List Message use API now Writting Message use API now ( to check ) String upgrade

--- app/src/main/java/org/esaip/projetandroid/ListesMessagesActivity.java
+++ app/src/main/java/org/esaip/projetandroid/ListesMessagesActivity.java
@@ -1,22 +1,5 @@
 package org.esaip.projetandroid;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -38,6 +21,22 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.SimpleAdapter;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class ListesMessagesActivity extends ActionBarActivity implements SearchView.OnQueryTextListener {
     private ListView listViewMessages;
@@ -96,7 +95,7 @@ public class ListesMessagesActivity extends ActionBarActivity implements SearchV
         searchView.setIconifiedByDefault(false);
         searchView.setOnQueryTextListener(this);
         searchView.setSubmitButtonEnabled(true);
-        searchView.setQueryHint("Search Here");
+        searchView.setQueryHint(getString(R.string.SearchQueryHint));
     }
 
     @Override
@@ -210,7 +209,7 @@ public class ListesMessagesActivity extends ActionBarActivity implements SearchV
     }
 
 
-    private class GetMessages extends AsyncTask<String, Void, String> {
+    private class GetMessages extends AsyncTask<String, Void, JSONArray> {
 
         @Override
         protected void onPreExecute() {
@@ -222,86 +221,107 @@ public class ListesMessagesActivity extends ActionBarActivity implements SearchV
         }
 
         @Override
-        protected String doInBackground(String... params) {
-
+        protected JSONArray doInBackground(String... params) {
+            JSONArray finalResult=null;
+            String responseText = "";
 
             try {
                 userBeingUsed = params[0].toString();
                 passwordBeingUsed = params[1].toString();
                 HttpGet get = new HttpGet();
-                URI uri = new URI("http://formation-android-esaip.herokuapp.com/messages/" + userBeingUsed + "/" + passwordBeingUsed);
+                URI uri = new URI("http://training.loicortola.com/parlez-vous-android/message/" + userBeingUsed + "/" + passwordBeingUsed);
                 get.setURI(uri);
                 DefaultHttpClient httpClient = new DefaultHttpClient();
                 HttpResponse httpResponse = httpClient.execute(get);
+
                 if (httpResponse.getStatusLine().getStatusCode() == 200) {
                     Log.d("[GET REQUEST]1", "HTTP Get succeeded");
 
-                    HttpEntity messageEntity = httpResponse.getEntity();
-                    InputStream is = messageEntity.getContent();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        response.append(line);
-                    }
+                    HttpResponse response; // some response object
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+                    StringBuilder builder = new StringBuilder();
+                    for (String line = null; (line = reader.readLine()) != null;) {
+                        builder.append(line).append("\n");
+                     }
+                    JSONTokener tokener = new JSONTokener(builder.toString());
+                    finalResult = new JSONArray(tokener);
+                    return finalResult;
+
                 }
             } catch (Exception e) {
                 Log.e("[GET REQUEST]2", e.getMessage());
             }
             Log.d("[GET REQUEST]3", response.toString());
 
-			/*            if(response.toString().matches("false"))
-            {
-                Toast.makeText(MainActivity.this, "Donn�es correctes !", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                Toast.makeText(MainActivity.this, "Donn�es incorrectes", Toast.LENGTH_SHORT).show();
-            }*/
 
 
-            return response.toString();
-
+            return finalResult;
         }
 
 
-        protected void onPostExecute(String answer) {
+        protected void onPostExecute(JSONArray answer) {
 
 
             spinner.setVisibility(View.GONE);
             rafraichir.setVisibility(View.VISIBLE);
             listViewMessages.setVisibility(View.VISIBLE);
             searchView.setVisibility(View.VISIBLE);
-            String[] listeMessagesChronologiques = answer.split(";|\\:");
-
-
-            List<String> arrayToList = Arrays.asList(listeMessagesChronologiques);
-            Collections.reverse(arrayToList);
-            listeMessagesChronologiques = (String[]) arrayToList.toArray();
-
-
-            String[][] listMessagesAntiChronologiques = new String[listeMessagesChronologiques.length / 2][2];
-
-            int message = 0;
-
-            for (int i = 0; i < listeMessagesChronologiques.length; i++) {
-                listMessagesAntiChronologiques[message][0] = listeMessagesChronologiques[i];
-                listMessagesAntiChronologiques[message][1] = listeMessagesChronologiques[i + 1].toUpperCase();
-                i++;
-                message++;
-            }
-
-
             List<HashMap<String, String>> liste = new ArrayList<HashMap<String, String>>();
-
             HashMap<String, String> element;
 
-            for (int i = 0; i < listMessagesAntiChronologiques.length; i++) {
+            JSONObject obj;
+            try {
+//                for (int count = answer.length(); count >= 0; count--) {
+                    for (int count = answer.length()-1; count >=0; count--) {
+                     obj = answer.getJSONObject(count);
 
-                element = new HashMap<String, String>();
-                element.put("text1", listMessagesAntiChronologiques[i][0]);
-                element.put("text2", listMessagesAntiChronologiques[i][1]);
-                liste.add(element);
+                    String login = obj.getString("login").toUpperCase();
+                    String message = obj.getString("message");
+                    element = new HashMap<String, String>();
+                    element.put("text1", login);
+                    element.put("text2", message);
+                    liste.add(element);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+
+
+
+
+
+
+//            String[] listeMessagesChronologiques = answer.split(";|\\:");
+//
+//
+//            List<String> arrayToList = Arrays.asList(listeMessagesChronologiques);
+//            Collections.reverse(arrayToList);
+//            listeMessagesChronologiques = (String[]) arrayToList.toArray();
+//
+//
+//            String[][] listMessagesAntiChronologiques = new String[listeMessagesChronologiques.length / 2][2];
+//
+//            int message = 0;
+//
+//            for (int i = 0; i < listeMessagesChronologiques.length; i++) {
+//                listMessagesAntiChronologiques[message][0] = listeMessagesChronologiques[i];
+//                listMessagesAntiChronologiques[message][1] = listeMessagesChronologiques[i + 1].toUpperCase();
+//                i++;
+//                message++;
+//            }
+
+
+//            List<HashMap<String, String>> liste = new ArrayList<HashMap<String, String>>();
+//
+//            HashMap<String, String> element;
+
+//            for (int i = 0; i < listMessagesAntiChronologiques.length; i++) {
+//
+//                element = new HashMap<String, String>();
+//                element.put("text1", listMessagesAntiChronologiques[i][0]);
+//                element.put("text2", listMessagesAntiChronologiques[i][1]);
+//                liste.add(element);
+//            }
 
 
             ListAdapter adapter = new SimpleAdapter(ListesMessagesActivity.context.getApplicationContext(), liste, android.R.layout.simple_list_item_2, new String[]{"text1", "text2"}, new int[]{android.R.id.text1, android.R.id.text2});
--- app/src/main/java/org/esaip/projetandroid/SigninActivity.java
+++ app/src/main/java/org/esaip/projetandroid/SigninActivity.java
@@ -1,9 +1,6 @@
 package org.esaip.projetandroid;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -22,47 +19,31 @@ import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SigninActivity extends ActionBarActivity {
     private EditText usernameField;
     private EditText passwordField;
     private TextView error_message;
     private ProgressBar progressRing;
-    private Button connect;
+    private Button signin_button;
     private Button clear;
     public static final String MyPREFERENCES = "MyPrefs";
     public static final String Name = "nameKey";
     public static final String Pass = "passKey";
     final String EXTRA_LOGIN = "username";
     final String EXTRA_PASS = "password";
-    SharedPreferences sharedpreferences;
     private String userBeingUsed;
     private String passwordBeingUsed;
 
@@ -77,6 +58,7 @@ public class MainActivity extends ActionBarActivity {
         super.onResume();
         Log.i("MainActivity", "onResume!");
     }
+
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
@@ -111,7 +93,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // Always call the superclass first
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signin_page);
 
 //        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
@@ -126,18 +108,9 @@ public class MainActivity extends ActionBarActivity {
 
         }
 
-        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
-        if (sharedpreferences.contains(Name)) {
-            usernameField.setText(sharedpreferences.getString(Name, ""));
-        }
-        if (sharedpreferences.contains(Pass)) {
-            passwordField.setText(sharedpreferences.getString(Pass, ""));
-        }
-
-
-        connect = (Button) findViewById(R.id.connect_button);
-        connect.setOnClickListener(
+        signin_button = (Button) findViewById(R.id.signin_button);
+        signin_button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -146,7 +119,7 @@ public class MainActivity extends ActionBarActivity {
                             setContentView(R.layout.activity_main);
                         } else {
 
-                            new ConnectTask().execute(usernameField.getText().toString(), passwordField.getText().toString());
+                            new SignInTask().execute(usernameField.getText().toString(), passwordField.getText().toString());
                         }
 
                     }
@@ -166,20 +139,13 @@ public class MainActivity extends ActionBarActivity {
 
         );
 
-        TextView textView = (TextView) findViewById(R.id.signin_textview);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new SignInTask().execute();
-
-            }
-        });
-
         Log.i("MainActivity", "onCreate!");
 
     }
 
+    public void onClick(View v) {
+
+    }
 
     public boolean checkFields() {
         if (passwordField.getText().toString().isEmpty() || usernameField.getText().toString().isEmpty()) {
@@ -221,7 +187,7 @@ public class MainActivity extends ActionBarActivity {
                 conf = res.getConfiguration();
                 conf.locale = locale;
                 res.updateConfiguration(conf, dm);
-                refresh = new Intent(this, MainActivity.class);
+                refresh = new Intent(this, SigninActivity.class);
                 startActivity(refresh);
                 return true;
             case R.id.French_Language:
@@ -233,7 +199,7 @@ public class MainActivity extends ActionBarActivity {
                 conf = res.getConfiguration();
                 conf.locale = locale;
                 res.updateConfiguration(conf, dm);
-                refresh = new Intent(this, MainActivity.class);
+                refresh = new Intent(this, SigninActivity.class);
                 startActivity(refresh);
                 return true;
             case R.id.Spanish_Language:
@@ -245,7 +211,7 @@ public class MainActivity extends ActionBarActivity {
                 conf = res.getConfiguration();
                 conf.locale = locale;
                 res.updateConfiguration(conf, dm);
-                refresh = new Intent(this, MainActivity.class);
+                refresh = new Intent(this, SigninActivity.class);
                 startActivity(refresh);
                 return true;
             default:
@@ -255,76 +221,94 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    public class ConnectTask extends android.os.AsyncTask<String, Void, Boolean> {
+    public class SignInTask extends android.os.AsyncTask<String, Void, Integer> {
         final String EXTRA_LOGIN = "username";
 
-        protected Boolean doInBackground(String[] params) {
+//        protected Boolean doInBackground(String[] params) {
+//            Log.i("doInBackground", "Begin!");
+//
+//            userBeingUsed = params[0].toString();
+//            passwordBeingUsed = params[1].toString();
+//            HttpClient httpclient = new DefaultHttpClient();
+//            HttpGet request = new HttpGet("http://formation-android-esaip.herokuapp.com/connect/" + userBeingUsed + "/" + passwordBeingUsed);
+////            Log.i("doInBackground", "httpRequestCreation!");
+////            Log.i("doInBackground", "http://formation-android-esaip.herokuapp.com/connect/" + user + "/" + pass);
+//
+//            try {
+////                Log.i("doInBackground", "Request pas envoyée!");
+//                HttpResponse response = httpclient.execute(request);
+////                Log.i("doInBackground", "Request envoyée!");
+//                String resp = new InputStreamToString().convert(response.getEntity().getContent());
+////                Log.i("doInBackground", "response = !" + resp);
+//                if (resp.equals("true")) {
+////                    Log.i("doInBackground", "response est VRAI");
+//                    return true;
+//                } else {
+////                    Log.i("doInBackground", "response est FAUX");
+//                    return false;
+//                }
+//            } catch (ClientProtocolException e) {
+//                // Handle exception
+//            } catch (IOException e) {
+//                // Handle exception
+//            }
+//
+//
+//            return null;
+//        }
+
+        protected Integer doInBackground(String[] params) {
             Log.i("doInBackground", "Begin!");
 
             userBeingUsed = params[0].toString();
             passwordBeingUsed = params[1].toString();
             HttpClient httpclient = new DefaultHttpClient();
-            HttpGet request = new HttpGet("http://training.loicortola.com/parlez-vous-android/connect/" + userBeingUsed + "/" + passwordBeingUsed);
-//            HttpGet request = new HttpGet("http://formation-android-esaip.herokuapp.com/connect/" + userBeingUsed + "/" + passwordBeingUsed);
-//            Log.i("doInBackground", "httpRequestCreation!");
-//            Log.i("doInBackground", "http://formation-android-esaip.herokuapp.com/connect/" + user + "/" + pass);
-
+            StringBuilder builder = new StringBuilder();
+            //            HttpPost post = new HttpPost("http://formation-android-esaip.herokuapp.com/register");
+//            HttpGet request = new HttpGet("http://training.loicortola.com/parlez-vous-android/register/" + userBeingUsed + "/" + passwordBeingUsed);
+            HttpPost post = new HttpPost("http://training.loicortola.com/parlez-vous-android/register/" + userBeingUsed + "/" + passwordBeingUsed);
             try {
-//                Log.i("doInBackground", "Request pas envoyée!");
-                HttpResponse response = httpclient.execute(request);
-//                Log.i("doInBackground", "Request envoyée!");
-//                 Log.i("doInBackground", "response = !" + resp);
-                StatusLine statusLine = response.getStatusLine();
-                StringBuilder builder = new StringBuilder();
+                post.setHeader("Content-Type", "application/json");
+                HttpResponse httpresponse = httpclient.execute(post);
+                StatusLine statusLine = httpresponse.getStatusLine();
                 int statusCode = statusLine.getStatusCode();
-                switch (statusCode) {
-                    case 200 :
-                       return true;
-                    case 400 :
-                        return false;
-                    case 404 :
-                        return false;
-                    default:
-                        return false;
+                Log.i("Recup status code ",""+statusCode);
+                return statusCode;
 
-                }
 
             } catch (ClientProtocolException e) {
-                // Handle exception
+                e.printStackTrace();
             } catch (IOException e) {
-                // Handle exception
+                e.printStackTrace();
             }
 
-
-            return null;
+            return 0;
         }
 
 
-
         @Override
-        protected void onPostExecute(Boolean o) {
-            super.onPostExecute(o);
+        protected void onPostExecute(Integer statusCode) {
+            super.onPostExecute(statusCode);
             progressRing.setVisibility(View.GONE);
-            if (o != null) {
-                if (o) {
-                    String n = usernameField.getText().toString();
-                    String p = passwordField.getText().toString();
-                    SharedPreferences.Editor editor = sharedpreferences.edit();
-                    editor.putString(Name, n);
-                    editor.putString(Pass, p);
-                    editor.commit();
 
-                    Intent intent = new Intent(MainActivity.this, ChoiceDisplayActivity.class);
-                    intent.putExtra(EXTRA_LOGIN, userBeingUsed);
-                    intent.putExtra(EXTRA_PASS, passwordBeingUsed);
-                    startActivity(intent);
-
-                } else {
-                    Toast.makeText(getApplicationContext(), getString(R.string.Log_Error_Message), Toast.LENGTH_SHORT).show();
-                }
+            switch (statusCode) {
+                case 200:
+                    Toast.makeText(getApplicationContext(), getString(R.string.Signin_Success_Message), Toast.LENGTH_SHORT).show();
+                    SigninActivity.this.finish();
+                    break;
+                case 400:
+                    Toast.makeText(getApplicationContext(), getString(R.string.Error_Invalid_Body_Objects), Toast.LENGTH_SHORT).show();
+                    break;
+                case 403:
+                    Toast.makeText(getApplicationContext(), getString(R.string.Authentification_Error), Toast.LENGTH_SHORT).show();
+                    break;
+                case 404:
+                    Toast.makeText(getApplicationContext(), getString(R.string.Signin_Error_Message_Not_Found), Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(getApplicationContext(), getString(R.string.Other_error), Toast.LENGTH_SHORT).show();
+                    break;
             }
-
-
         }
 
 
@@ -333,37 +317,8 @@ public class MainActivity extends ActionBarActivity {
 
             Log.i("onPreExecute", "Pré roue de chargement");
             progressRing.setVisibility(View.VISIBLE);
-            super.onPreExecute();
-        }
-    }
-
-    public class SignInTask extends android.os.AsyncTask<String, Void, Boolean> {
-        final String EXTRA_LOGIN = "username";
-
-        protected Boolean doInBackground(String[] params) {
-            Log.i("doInBackground", "Begin!");
-            return true;
-        }
 
 
-
-        @Override
-        protected void onPostExecute(Boolean o) {
-            super.onPostExecute(o);
-            progressRing.setVisibility(View.GONE);
-
-            Intent intent = new Intent(MainActivity.this,  SigninActivity.class);
-            startActivity(intent);
-
-
-        }
-
-
-        @Override
-        protected void onPreExecute() {
-
-            Log.i("onPreExecute", "Pré roue de chargement");
-            progressRing.setVisibility(View.VISIBLE);
             super.onPreExecute();
         }
     }
